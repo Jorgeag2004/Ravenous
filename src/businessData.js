1a58a5519520db54532f080317d9f35528ac7347
Jorge Gonzalez <jorgeag2004@icloud.com>
2025-05-19 22:53:34 -0400

    Added more sample spots

--- src/businessData.js
+++ src/businessData.js
@@ -42,6 +42,50 @@ const Businesses = [
         category: "Fried Chicken",
         rating: 5.0,
         reviewCount: 98
+    },
+    {
+        imageSrc: "https://dynl.mktgcdn.com/p/Thy5snm_6OPuLoBOfncnTsgKv7aafNqmFpamFHB6v-I/619x619.jpg",
+        name: "KFC",
+        address: "2031 Elm Street",
+        city: "Cedar Rapids",
+        state: "IA",
+        zipcode: "52402",
+        category: "Fried Chicken",
+        rating: 4.2,
+        reviewCount: 184
+    },
+    {
+        imageSrc: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/5/14/1/FNM_060114-Hattie-Bs-Hot-Chicken-Recipe_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1400182242803.webp",
+        name: "Hattie B's Hot Chicken",
+        address: "112 19th Ave S",
+        city: "Nashville",
+        state: "TN",
+        zipcode: "37203",
+        category: "Hot Chicken",
+        rating: 4.8,
+        reviewCount: 240
+    },
+    {
+        imageSrc: "https://www.littlerock.com/imager/files_idss_com/C349/a893018c-0f66-4db8-8916-5d83bba65c5f_e45adf5f6bc0c5c2a30a39868f44eab6.png",
+        name: "Gus's World Famous Fried Chicken",
+        address: "310 S Front St",
+        city: "Memphis",
+        state: "TN",
+        zipcode: "38103",
+        category: "Fried Chicken",
+        rating: 4.7,
+        reviewCount: 132
+    },
+    {
+        imageSrc: "https://mma.prnewswire.com/media/2388426/258_RTP_Bonchon_2_16_24_X2.jpg",
+        name: "Bonchon Chicken",
+        address: "45 Harvard Ave",
+        city: "Allston",
+        state: "MA",
+        zipcode: "02134",
+        category: "Korean Fried Chicken",
+        rating: 4.6,
+        reviewCount: 89
     }
 ];
 
