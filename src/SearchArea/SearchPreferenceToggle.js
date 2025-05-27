4121ec74a348630ac3aa99781ba08467f8880aeb
Jorge Gonzalez <jorgegonzalez@Jorges-Mac-Studio.local>
2025-05-27 18:52:16 -0400

    Edited search preference button labels

--- src/SearchArea/SearchPreferenceToggle.js
+++ src/SearchArea/SearchPreferenceToggle.js
@@ -10,13 +10,13 @@ function SearchPreferenceToggle() {
     return (
         <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
             <ToggleButton id="tbg-btn-1" value={1}>
-                Option 1
+                Best Match
             </ToggleButton>
             <ToggleButton id="tbg-btn-2" value={2}>
-                Option 2
+               Highest Rated
             </ToggleButton>
             <ToggleButton id="tbg-btn-3" value={3}>
-                Option 3
+               Most Reviewed
             </ToggleButton>
         </ToggleButtonGroup>
     )
