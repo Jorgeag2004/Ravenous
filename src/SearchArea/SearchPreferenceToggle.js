89872809707ae40e619a088262564ac448cdb170
Jorge Gonzalez <jorgegonzalez@Jorges-Mac-Studio.local>
2025-05-28 14:51:29 -0400

    Changed values of radio buttons to match their lables

--- src/SearchArea/SearchPreferenceToggle.js
+++ src/SearchArea/SearchPreferenceToggle.js
@@ -3,14 +3,14 @@ import {ToggleButtonGroup} from "react-bootstrap";
 
 function SearchPreferenceToggle(props) {
   return (
-      <ToggleButtonGroup onChange={props.handleSortingChoice} type={"radio"} name={"Search Preferences"} defaultValue={1}>
-          <ToggleButton id={"radio-1"} value={1}>
+      <ToggleButtonGroup onChange={props.handleSortingChoice} type={"radio"} name={"Search Preferences"} defaultValue={"Best Match"}>
+          <ToggleButton id={"radio-1"} value={"Best Match"}>
               Best Match
           </ToggleButton>
-          <ToggleButton id={"radio-2"} value={2}>
+          <ToggleButton id={"radio-2"} value={"Highest Rated"}>
              Highest Rated
           </ToggleButton>
-          <ToggleButton id={"radio-3"} value={3}>
+          <ToggleButton id={"radio-3"} value={"Most Reviewed"}>
              Most Reviewed
           </ToggleButton>
       </ToggleButtonGroup>
